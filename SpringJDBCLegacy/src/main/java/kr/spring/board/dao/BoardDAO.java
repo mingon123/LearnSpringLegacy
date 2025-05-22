708cd0cf5961f7e50e407efe9ee355494bc3f913
mingon123 <mingon0125@gmail.com>
2025-05-22 18:21:48 +0900

    스프링 JDBC - 기본 설정

--- SpringJDBCLegacy/src/main/java/kr/spring/board/dao/BoardDAO.java
+++ SpringJDBCLegacy/src/main/java/kr/spring/board/dao/BoardDAO.java
@@ -7,8 +7,7 @@ import kr.spring.board.vo.BoardVO;
 public interface BoardDAO {
 	public void insertBoard(BoardVO board);
 	public int getBoardCount();
-	public List<BoardVO> getBoardList(
-			           int startRow, int endRow);
+	public List<BoardVO> getBoardList(int startRow, int endRow);
 	public BoardVO getBoard(int num);
 	public void updateBoard(BoardVO board);
 	public void deleteBoard(int num);
--- SpringJDBCLegacy/src/main/java/kr/spring/board/dao/BoardDAOImpl.java
+++ SpringJDBCLegacy/src/main/java/kr/spring/board/dao/BoardDAOImpl.java
@@ -1,5 +1,45 @@
 package kr.spring.board.dao;
 
-public class BoardDAOImpl {
+import java.util.List;
 
+import kr.spring.board.vo.BoardVO;
+
+public class BoardDAOImpl implements BoardDAO{
+
+	@Override
+	public void insertBoard(BoardVO board) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getBoardCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<BoardVO> getBoardList(int startRow, int endRow) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public BoardVO getBoard(int num) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void updateBoard(BoardVO board) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteBoard(int num) {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
